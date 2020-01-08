62cb4ca32541a28eed99a3bfd14cac3582319f8a
radoslavmt <valsodarbg@gmail.com>
2020-01-09 01:11:10 +0200

    optimized check cow function.

--- bac.js
+++ bac.js
@@ -11,6 +11,21 @@ let p1n = document.getElementById("my1"),
     pl1 = document.getElementById("pl1").innerText,
     pl2 = document.getElementById("pl2").innerText;
 
+function checkALL(num){
+    let check=0;
+    for (let i =0; i < 4; i++){
+        for (let j = 0; j < 4; j++){
+            if(num.value[i] === num.value[j] && i !== j){
+                if(check > 0){
+                    msg.innerText="Please Kiss Me";
+                   return false;
+                }
+                check++;
+            }
+        }
+    }
+    return true;
+}
 
 function checkBull(rec,res,text,player) {
     let result=0;
@@ -19,150 +34,41 @@ function checkBull(rec,res,text,player) {
             result++;
         }
     }
-    text.innerHTML += rec + " - "+result+" bulls \n";
+    text.innerHTML += rec + " - "+result+" bulls ";
     text.scrollTop = text.scrollHeight;
 
     if(result === 4) {
         msg.innerHTML = player + " YOU WIN";
     }
-}
+}1
 
-function checkCowP1() {
-    if (    // 4 cows combinations//
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1]) {
-            p1t.innerHTML += "4 cows" + "\n" + "\n";
-            p1t.scrollTop = p1t.scrollHeight;
-    } else if (    // 3 cows with index 1, 2 and 3 //
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[2] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-            // 3 cows with index 2, 3 and 4 //
-        p1c.value[0] === p2n.value[1] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-            // 3 cows with index 1, 3 and 4 //
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        // 3 cows with index 1, 2 and 4 //
-        p1c.value[0] === p2n.value[1] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[1]) {
-            p1t.innerHTML += "3 cows" + "\n" + "\n";
-            p1t.scrollTop = p1t.scrollHeight;
-    } else if (    // 2 cows with index 1 and 2 //
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[1] ||
-            // 2 cows with index 3 and 4 //
-        p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[3] || 
-            // 2 cows with index 2 and 3 //
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[2] === p2n.value[1] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[3] === p2n.value[1] ||
-            // 2 cows with index 1 and 3 //
-        p1c.value[0] === p2n.value[2] && p1c.value[1] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[2] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[2] === p2n.value[0] && p1c.value[3] === p2n.value[2] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[2] && p1c.value[3] === p2n.value[0] ||
-            // 2 cows with index 2 and 4 //
-        p1c.value[0] === p2n.value[1] && p1c.value[1] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[1] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[1] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-        p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[1] ||
-            // 2 cows with index 1 and 4 //
-        p1c.value[1] === p2n.value[0] && p1c.value[2] === p2n.value[3] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[1] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[2] === p2n.value[0] ||
-        p1c.value[2] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[1] === p2n.value[3] && p1c.value[3] === p2n.value[0] ||
-        p1c.value[0] === p2n.value[3] && p1c.value[3] === p2n.value[0]) {
-            p1t.innerHTML += "2 cows" + "\n" + "\n";
-            p1t.scrollTop = p1t.scrollHeight;
-    } else if ( // 1 cow combinations //
-        p1c.value[0] === p2n.value[1] || p1c.value[0] === p2n.value[2] || p1c.value[0] === p2n.value[3] ||
-        p1c.value[1] === p2n.value[0] || p1c.value[1] === p2n.value[2] || p1c.value[1] === p2n.value[3] ||
-        p1c.value[2] === p2n.value[0] || p1c.value[2] === p2n.value[1] || p1c.value[2] === p2n.value[3] ||
-        p1c.value[3] === p2n.value[0] || p1c.value[3] === p2n.value[1] || p1c.value[3] === p2n.value[2]) {
-            p1t.innerHTML += "1 cow" + "\n" + "\n";
-            p1t.scrollTop = p1t.scrollHeight;
-    } else {
-        p1t.innerHTML += "0 cows" + "\n" + "\n";
-        p1t.scrollTop = p1t.scrollHeight; 
+function checkCow(rec,res,text) {
+    let result=0;
+    for (let i=0; i < 4; i++) {
+        for (let j=0; j < 4; j++){
+            if (rec[i] === res[j] && i !== j){
+                result++;
+            }
+        }
     }
+    text.innerHTML += result+" cows\n";
+    text.scrollTop = text.scrollHeight;
 }
 
 bt1.addEventListener('click', function() {
     msg.innerHTML = "Its Player 2 turn".fontsize(20);
-    checkBull(p1c.value, p2n.value,p1t,pl1);
-    // checkCowP1();
+    if (checkALL(p1c) && checkALL(p2n)) {
+        checkBull(p1c.value, p2n.value,p1t,pl1);
+        checkCow(p1c.value, p2n.value,p1t);
+    }
 }, false);
 
 bt2.addEventListener('click', function() {
     msg.innerHTML = "Its Player 1 turn".fontsize(20);
-    checkBull(p2c.value, p1n.value,p2t,pl2);
-    // checkCowP2();
+    if (checkALL(p2c) && checkALL(p1n)) {
+        checkBull(p2c.value, p1n.value,p2t,pl2);
+        checkCow(p2c.value, p1n.value,p2t);
+    }
 }, false);
 
 start.addEventListener('click', function() {
